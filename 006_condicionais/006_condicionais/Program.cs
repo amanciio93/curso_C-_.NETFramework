8f385d83d7b2934f6f4bceeceec3504ac6375ba7
amanciio93 <amanciio93@gmail.com>
2025-02-06 01:21:53 -0300

    Condicionais e operadores

---
+++ 006_condicionais/006_condicionais/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006_condicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Condicionais:
+                    > Maior
+                    < Menor
+                    >= Maior ou igual
+                    <= Menor ou igual
+                    == Igual
+                    != Diferente             
+             */
+
+            int idade = 18;
+
+            if (idade > 18)
+            {
+                Console.WriteLine("maioridade");
+            }
+            else if (idade < 18) {
+                Console.WriteLine("menoridade");
+            }
+            else {
+                Console.WriteLine("Tem 18 anos.");
+            }
+        }
+    }
+}
---
+++ 007_operadores_logicos/007_operadores_logicos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_operadores_logicos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // && Operador and;
+            // || Operador or;
+
+            int idade = 17;
+            char sexo = 'm';
+
+            if (sexo == 'm' && idade >= 18)
+            {
+                Console.WriteLine("Já deve se alistar!");
+            }
+            else if (sexo == 'f' && idade >= 18)
+            {
+                Console.WriteLine("Alistamento opcional!");
+            } else if ((sexo == 'm' || sexo == 'f') && idade < 18) {
+                Console.WriteLine("Parabéns pelo interesse!");
+            }
+            else if (sexo != 'm' && sexo != 'f') {
+                Console.WriteLine("Sexo inexistente!");
+            }
+            else
+            {
+                Console.WriteLine("Ainda não tem idade para se alistar!");
+            }
+
+            
+        }
+    }
+}
