4e36e62267a5e1e754ac2ec922e78722266eefd6
amanciio93 <amanciio93@gmail.com>
2025-02-06 21:42:34 -0300

    trabalhando com funções

---
+++ 008_funcoes/008_funcoes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008_funcoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Executando a função declarada;
+            mostraNome();
+            gerarPreco("Garrafa Stanley", 55); // 55 é o parametro;
+        }
+
+        //Declarando funções - Devem ser declaradas dentro do internal class Program;
+        //static + retorno + nome da função
+        static void mostraNome() {
+            Console.WriteLine("Jonathan Amancio");
+        }
+
+        //Passando parametros e argumentos;
+        static void gerarPreco(string nome, int preco) { // "int preco" recebe o argumento que é o valor enviado pelo parametro preco na chamada da função;
+            Console.WriteLine("Nome do produto: " + nome);
+            Console.WriteLine("Preço: " + preco);
+        }
+
+    }
+}
