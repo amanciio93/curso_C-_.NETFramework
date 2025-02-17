d4da90ba4b9247a9806a2577be146416a59304b4
amanciio93 <amanciio93@gmail.com>
2025-02-16 23:02:52 -0300

    Conhecendo arrays

--- 010_arrays/010_arrays/Program.cs
+++ 010_arrays/010_arrays/Program.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _010_arrays
+namespace _010_Arrays
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ARRAYS: \n\n");
 
+            string[] produtos = new string[5] {
+                "Gran Turismo", //0
+                "Need for speed", //1
+                "Counter Strike", //2
+                "GTA SanAndreas", //3
+                "TribalWars" //4
+            };
+
+            Console.WriteLine(produtos[0]);
+            Console.WriteLine(produtos[1]);
+            Console.WriteLine(produtos[2]);
+            Console.WriteLine(produtos[3]);
+            Console.WriteLine(produtos[4]);
+
+            //Alterando um valor dentro do Array;
+            produtos[3] = "GTA VI";
+            Console.WriteLine(produtos[3]);
 
         }
     }
--- 010_arrays/010_arrays/Properties/AssemblyInfo.cs
+++ 010_arrays/010_arrays/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
-[assembly: AssemblyTitle("010_arrays")]
+[assembly: AssemblyTitle("010_Arrays")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("010_arrays")]
+[assembly: AssemblyProduct("010_Arrays")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
-[assembly: Guid("d497414a-5552-425c-a0a1-3b4d068e1bf5")]
+[assembly: Guid("0f377e26-764b-4adb-9168-ba3c920b64ea")]
 
 // As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
